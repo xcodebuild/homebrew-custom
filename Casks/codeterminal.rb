274cbd1c3469400398b96c9d3fde32529159fd19
shejinxin <shejinxin@corp.netease.com>
2022-04-08 15:19:28 +0800

    casks works

--- Casks/codeterminal.rb
+++ Casks/codeterminal.rb
@@ -1,6 +1,6 @@
 cask "codeterminal" do
-  # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "0.0.1"
+  version :latest
+  sha256 :no_check
   if Hardware::CPU.intel?
     url "https://nightly.link/xcodebuild/CodeTerminal/workflows/nightly/main/CodeTerminalNightly-macOS-Intel.zip"
   else
@@ -10,9 +10,17 @@ cask "codeterminal" do
   desc "Cross platform terminal app from Visual Studio Code."
   homepage "https://github.com/xcodebuild/CodeTerminal"
 
+  depends_on formula: "p7zip"
   auto_updates true
 
-  app "CodeTerminal.app"
+  livecheck do
+    url "https://cdn.jsdelivr.net/gh/xcodebuild/codeterminal@main/package.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["version"]
+    end
+  end
+
+  app "./CodeTerminal.app"
 
   zap trash: [
     "~/Library/Application Support/CodeTerminal",
