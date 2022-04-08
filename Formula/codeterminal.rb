673b7037bcc01c6796f517c79b5fb2722db3afad
shejinxin <shejinxin@corp.netease.com>
2022-04-08 13:15:51 +0800

    inital commit

---
+++ Formula/codeterminal.rb
@@ -0,0 +1,20 @@
+cask "codeterminal" do
+  # NOTE: "2" is not a version number, but an intrinsic part of the product name
+  version "0.0.1"
+  if Hardware::CPU.intel?
+    url "https://nightly.link/xcodebuild/CodeTerminal/workflows/nightly/main/CodeTerminalNightly-macOS-Intel.zip"
+  else
+    url "https://nightly.link/xcodebuild/CodeTerminal/workflows/nightly/main/CodeTerminalNightly-macOS-M1.zip"
+  end
+  name "CodeTerminal"
+  desc "Cross platform terminal app from Visual Studio Code."
+  homepage "https://github.com/xcodebuild/CodeTerminal"
+
+  auto_updates true
+
+  app "CodeTerminal.app"
+
+  zap trash: [
+    "~/Library/Application Support/CodeTerminal",
+  ]
+end
--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # Custom Homebrew Formula
 
-Install at your own risk
-
-* s3cmd 1.6
-* cerebro 0.9.2 (from https://github.com/lmenezes/cerebro)
 
 ## How do I install these formulae?
 ```
-brew tap inkstak/custom
-brew install s3cmd@1.6
-brew install cerebro
+brew tap xcodebuild/custom
+brew install codeterminal
 ```
