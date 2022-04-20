3ef8230100006f445f5373f26c49ce15a799166b
shejinxin <shejinxin@corp.netease.com>
2022-04-20 10:01:42 +0800

    update cask

--- Casks/codeterminal.rb
+++ Casks/codeterminal.rb
@@ -2,9 +2,9 @@ cask "codeterminal" do
   version :latest
   sha256 :no_check
   if Hardware::CPU.intel?
-    url "https://nightly.link/xcodebuild/CodeTerminal/workflows/nightly/main/CodeTerminalNightly-macOS-Intel.zip"
+    url "https://nightly.link/xcodebuild/CodeTerminal/workflows/release/release/CodeTerminalNightly-macOS-Intel.zip"
   else
-    url "https://nightly.link/xcodebuild/CodeTerminal/workflows/nightly/main/CodeTerminalNightly-macOS-M1.zip"
+    url "https://nightly.link/xcodebuild/CodeTerminal/workflows/release/release/CodeTerminalNightly-macOS-M1.zip"
   end
   name "CodeTerminal"
   desc "Cross platform terminal app from Visual Studio Code."
